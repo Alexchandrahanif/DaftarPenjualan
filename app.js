64e449a3fb7803f8a9fd25756f3c6b99118eb3e0
Alex Chandra Hanif <alexchandra1998@gmail.com>
2023-10-30 16:38:40 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+require("dotenv").config();
+require("dotenv").config({ path: `.env.local`, override: true });
+
 const router = require("./routes");
 const handleError = require("./middleware/handleError");
+
+const { Sequelize } = require("sequelize");
+
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Jika Anda ingin mematikan validasi sertifikat (dalam pengembangan)
+      },
+    },
+  });
+} else {
+  const config = require("./config/config.json")[process.env.NODE_ENV];
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+  });
+}
 
 const cors = require("cors");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
